4810df923bd69e9ec0be4e0db5e36ad631d12cdd
whiteskillae <whiteskillae@gmail.com>
2026-08-22 21:54:56 +0400

    invalid requests

--- run.ps1
+++
@@ -1,5 +0,0 @@
-# Run Backend
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd backend; npm run dev"
-
-# Run Frontend
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd frontend; npm run dev"
