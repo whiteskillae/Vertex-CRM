8f16d46f934de43f529d2789f44182342102d9a6
whiteskillae <whiteskillae@gmail.com>
2026-08-22 21:52:34 +0400

    invalid requests

--- backend/src/routes/calendarRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createNote, getNotes } = require('../controllers/calendarController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/', protect, createNote);
-router.get('/', protect, getNotes);
-
-module.exports = router;
